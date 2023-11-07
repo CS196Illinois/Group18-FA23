ad1a96648a9950c7d542f201bf30b993cb60edc7
Aruv <adand2765@gmail.com>
2023-11-07 17:19:43 -0600

    updated app.py

--- Project/app.py
+++ Project/app.py
@@ -41,22 +41,32 @@ def getTracks():
         if (len(items) < 50):
             break
         return str(all_songs)
-
-def get_token():
-    token_info = session.get(TOKEN_INFO, None)
-    if not token_info:
-        raise "exception"
-    now = int(time.time())
     
-    is_expired = token_info['expires_at'] - now < 60
-    if (is_expired):
+# check to see if token is valid, gets new token if not
+def get_token():
+    token_valid = False
+    token_info = session.get("token_info", {})
+
+    #Check if sessions has token stored
+    if not (session.get("token_info", False)):
+        token_valid = False
+        return token_info, token_valid
+    
+    #Check if token has expired
+    now = int(time.time())
+    is_token_expired = session.get('token_info').get('expires_at') - now < 60
+
+    # Refreshing token if it has expired
+    if (is_token_expired):
         sp_oauth = create_spotify_oauth()
-        token_info = sp_oauth.refresh_access_token(token_info['refresh_token'])
-    return token_info
+        token_info = sp_oauth.refresh_access_token(session.get('token_info').get('refresh_token'))
+
+    token_valid = True
+    return token_info, token_valid
 
 def create_spotify_oauth():
     return SpotifyOAuth(
-        client_id="76dff0cbe4d74670bd7a33dc3c4075bc",
-        client_secret="1401e59d1b354b849ce38a342f951d14",
+        client_id="id",
+        client_secret="id",
         redirect_uri=url_for('redirectPage', _external=True),
         scope="user-library-read")
